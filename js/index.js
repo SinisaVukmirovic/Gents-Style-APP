c471683dfe681b34ff05a5a7f72e7ea2936c26ac
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-02-26 23:06:18 +0100

    Trying out some ideas

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const items = {
+    vests: document.querySelectorAll('#vests .slider .item'),
+    cardigans: document.querySelectorAll('#cardigans .slider .item'),
+    watches: document.querySelectorAll('#watches .slider .item'),
+}
+
+console.log(items.vests);
